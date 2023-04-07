45c2835ae50b181cc88c28defcbf8fbe2b604663
JennyPersichini <nenny1096@gmail.com>
2023-04-07 10:58:00 +0200

    Raccolta dati

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-function buttonForm(event) {
+//Prima di tutto raccogliamo i dati necessari per fare i vari calcoli
 
+let backend = 20.50;    //All'ora
+let frontend = 15.30;   //All'ora
+let analisi = 33.60;    //All'ora
+
+let codiciDisponibili = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+let sconto = 0.25;
+
+function buttonForm(event) {
+    event.preventDefault();
+
+    //Nel click quindi bisogna calcolare il tipo di lavoro * le ore richieste, ed applicare un codice sconto (se disponibile!).
 }
